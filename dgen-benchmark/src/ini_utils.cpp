fffee4494b0c9bfc32ca94fa2b9eef5e7fb4569c
Adrian <a.kulawik8@gmail.com>
2022-08-12 12:49:18 +0200

    Add ini file management functions

---
+++ dgen-benchmark/src/ini_utils.cpp
@@ -0,0 +1,83 @@
+#include "ini_utils.hpp"
+#include <fstream>
+#include <iostream>
+
+namespace ini
+{
+	std::string Trim(const std::string& str)
+	{
+		static constexpr auto whitespaces = " \t\n\r\f\v";
+
+		const auto first = str.find_first_not_of(whitespaces);
+		if (first == std::string::npos) return "";
+
+		const auto last = str.find_last_not_of(whitespaces);
+		return str.substr(first, last - first + 1);
+	}
+
+	void Load(container_type& container, const std::string& path)
+	{
+		std::ifstream file(path);
+		if (!file.good()) return;
+
+		std::string line;
+		section_type* section = &(container[""]);
+
+		while (!file.eof())
+		{
+			std::getline(file, line);
+			size_t a, b;
+
+			a = line.find_first_of(";#");
+			if (a != std::string::npos)
+				line.erase(a);
+
+			a = line.find_first_of('[');
+			if (a != std::string::npos)
+			{
+				b = line.find_last_of(']');
+				if (b != std::string::npos && a < b)
+				{
+					section = &(container[Trim(line.substr(a + 1, b - a - 1))]);
+					continue;
+				}
+			}
+
+			a = line.find_first_of('=');
+			if (a != std::string::npos)
+			{
+				const std::string key = Trim(line.substr(0, a));
+				if (!key.empty())
+					(*section)[key] = Trim(line.substr(a + 1));
+			}
+		}
+	}
+
+	void Save(const container_type& container, const std::string& path)
+	{
+		std::ofstream file(path);
+		if (!file.good()) return;
+
+		bool next = false;
+		for (auto& [sectionName, section] : container)
+		{
+			if (next) file << '\n';
+			next = true;
+
+			file << '[' << sectionName << "]\n";
+
+			for (auto& [key, value] : section)
+				file << key << " = " << value << '\n';
+		}
+	}
+
+	void UserInput(section_type& section, const std::string& key)
+	{
+		std::cout << ' ' << key << " = ";
+
+		std::string input;
+		std::getline(std::cin, input);
+
+		section[key] = Trim(input);
+	}
+}
---
+++ dgen-benchmark/src/ini_utils.hpp
@@ -0,0 +1,78 @@
+#pragma once
+#include <map>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+
+namespace ini
+{
+	using section_type = std::map<std::string, std::string>;
+	using container_type = std::map<std::string, section_type>;
+
+	std::string Trim(const std::string& str);
+
+	void Load(container_type& container, const std::string& path);
+	void Save(const container_type& container, const std::string& path);
+	void UserInput(section_type& section, const std::string& key);
+
+	template <typename Type>
+	Type Convert(const std::string& str)
+	{
+		if constexpr (std::is_same_v<Type, bool>)
+		{
+			switch (str.at(0))
+			{
+			case '0': case 'f': case 'F': case 'n': case 'N':
+				return false;
+
+			case '1': case 't': case 'T': case 'y': case 'Y':
+				return true;
+
+			default:
+				throw std::invalid_argument("invalid_argument");
+			}
+		}
+		else if constexpr (std::is_same_v<Type, std::string>) { return str; }
+		else if constexpr (std::is_floating_point_v<Type>) { return static_cast<Type>(std::stof(str)); }
+		else if constexpr (std::is_integral_v<Type>) { return static_cast<Type>(std::stoi(str)); }
+		else { static_assert(!sizeof(Type*)); }
+	}
+
+	template <typename Type>
+	void Get(section_type& section, const std::string& key, Type& out, bool req, bool (*check)(const Type&) = nullptr)
+	{
+		while (true)
+		{
+			const auto iter = section.find(key);
+			if (iter != section.end() && iter -> second.size() > 0)
+			{
+				try
+				{
+					Type temp = Convert<Type>(iter -> second);
+					if (check == nullptr || check(temp))
+					{
+						out = std::move(temp);
+						break;
+					}
+				}
+				catch (...) {}
+			}
+
+			if (!req)
+				break;
+
+			UserInput(section, key);
+		}
+	}
+
+	template <typename Type>
+	void Get(container_type& container, const std::string& sectionName, const std::string& key, Type& out, bool req, bool (*check)(const Type&) = nullptr)
+	{
+		const auto iter = container.find(sectionName);
+
+		if (iter != container.end())
+			Get<Type>(iter -> second, key, out, req, check);
+		else if (req)
+			Get<Type>(container[sectionName], key, out, req, check);
+	}
+}
