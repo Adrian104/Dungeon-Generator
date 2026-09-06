b8c11b2c54460d92c1d24ac4a51e94de1e6a132a
Adrian Kulawik <a.kulawik8@gmail.com>
2026-09-06 14:40:03 +0200

    Begin dgen-app rewrite

--- dgen-app/src/main.cpp
+++ dgen-app/src/main.cpp
@@ -1,21 +1,22 @@
-// SPDX-FileCopyrightText: Copyright (c) 2023 Adrian Kulawik
+// SPDX-FileCopyrightText: Copyright (c) 2026 Adrian Kulawik
 // SPDX-License-Identifier: MIT
 
-#include "app.hpp"
-#include <cstdlib>
-#include <iostream>
-#include <stdexcept>
+#include "pch.hpp"
 
 int main(int, char**)
 {
 	try
 	{
-		Application app;
-		app.Run();
+
 	}
 	catch (const std::exception& error)
 	{
 		std::cerr << error.what() << '\n';
+		return EXIT_FAILURE;
+	}
+	catch (...)
+	{
+		std::cerr << "unknown error\n";
 		return EXIT_FAILURE;
 	}
 
---
+++ dgen-app/src/pch.hpp
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 Adrian Kulawik
+// SPDX-License-Identifier: MIT
+
+#pragma once
+#define IMGUI_DEFINE_MATH_OPERATORS
+
+#include <imgui.h>
+#include <SDL3/SDL.h>
+#include <backends/imgui_impl_sdl3.h>
+#include <backends/imgui_impl_sdlrenderer3.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
